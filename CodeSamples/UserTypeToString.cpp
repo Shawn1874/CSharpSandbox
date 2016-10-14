5b3e03e3cf6847cc0275c6ec1c54b6dab7b70209
Shawn1874 <kempofighter@gmail.com>
2016-10-13 17:53:15 -0700

    Add function header to operator<< implementation

--- CodeSamples/UserTypeToString.cpp
+++ CodeSamples/UserTypeToString.cpp
@@ -10,6 +10,9 @@ UserTypeToString::~UserTypeToString()
 {
 }
 
+/* when streaming an instance of this type to an ostream of any kind this function will build the string 
+   containing the state of the object.
+*/
 std::ostream& operator<<(std::ostream& os, const UserTypeToString& obj)
 {
     // write obj to stream
