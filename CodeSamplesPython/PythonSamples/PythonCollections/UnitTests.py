4247e8b58118d5aafbc55e4b261450c4fb8cd4c8
sfox <kempofighter@gmail.com>
2017-06-04 15:39:21 -0700

    added unit test for list::extend method

--- CodeSamplesPython/PythonSamples/PythonCollections/UnitTests.py
+++ CodeSamplesPython/PythonSamples/PythonCollections/UnitTests.py
@@ -15,5 +15,19 @@ class CollectionTests(unittest.TestCase):
         container.append(anotherFruit)
         self.assertIn(anotherFruit, container)
 
+    def test_extend(self):
+        container = [1, 3, 5]
+        self.assertNotIn(7, container)
+        self.assertNotIn(9, container)
+        container += [7, 9]
+        self.assertIn(7, container)
+        self.assertIn(9, container)
+        self.assertNotIn(11, container)
+        self.assertNotIn(13, container)
+        container.extend([11, 13])
+        self.assertIn(11, container)
+        self.assertIn(13, container)
+
+
 if __name__ == '__main__':
     unittest.main()
