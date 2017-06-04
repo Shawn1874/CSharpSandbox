7181e8f6fef090c41cb2c02ba12d78cf8cb2383d
sfox <kempofighter@gmail.com>
2017-06-04 16:09:28 -0700

    add unit test for methods of list that remove things

--- CodeSamplesPython/PythonSamples/PythonCollections/UnitTests.py
+++ CodeSamplesPython/PythonSamples/PythonCollections/UnitTests.py
@@ -28,6 +28,15 @@ class CollectionTests(unittest.TestCase):
         self.assertIn(11, container)
         self.assertIn(13, container)
 
+    def test_remove(self):
+        container = [1, 3, 6, 5, 7, 9, 11]
+        self.assertTrue(6 in container)
+        container.remove(6)
+        self.assertNotIn(6, container)
+        self.assertTrue(3 in container)
+        value = container.pop(1)
+        self.assertEqual(value, 3)
+        self.assertNotIn(3, container)
 
 if __name__ == '__main__':
     unittest.main()
