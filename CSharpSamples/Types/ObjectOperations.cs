c653475673f3d30f8ed1dcbe46dc6f150fde323e
sfox <kempofighter@gmail.com>
2017-10-15 18:10:50 -0700

    Added projects and new tests

---
+++ CSharpSamples/Types/ObjectOperations.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Types
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public Decimal Amount { get; set; }
+
+        public Invoice(int id, string descr, Decimal amount)
+        {
+            this.Id = id;
+            this.Amount = amount;
+            this.Description = descr;
+        }
+
+        public Invoice Clone()
+        {
+            Invoice clone = (Invoice)this.MemberwiseClone();
+            return clone;
+        }
+    };
+
+    [TestFixture]
+    public class ObjectOperations
+    {
+        [Test]
+        public void EqualsTests()
+        {
+            var fenceRepair = new Invoice(1, "Patio Fence Repair", 200m);
+            var plumbingRepair = new Invoice(2, "Kitchen Sink Repair", 350.75m);
+            var plumbingRepair2 = plumbingRepair;
+            Assert.That(ReferenceEquals(fenceRepair, plumbingRepair) == false);
+            Assert.That(ReferenceEquals(plumbingRepair2, plumbingRepair));
+
+            // After cloning the string properties are still the same reference until one of the strings 
+            // is reassigned to a new string.
+            var fenceRepair2 = fenceRepair.Clone();
+            Assert.That(ReferenceEquals(fenceRepair.Description, fenceRepair2.Description));
+
+            fenceRepair2.Description = "Fence Repair";
+            Assert.That(!ReferenceEquals(fenceRepair.Description, fenceRepair2.Description));
+
+            // for reference types, Equals and ReferenceEquals tests for the same thing, but the
+            // ID property is an integer which is a value type
+            Assert.That(ReferenceEquals(fenceRepair, fenceRepair2) == false);
+            Assert.That(Equals(fenceRepair2, fenceRepair) == false);
+            Assert.That(Equals(fenceRepair2.Id, fenceRepair.Id));
+
+            Assert.That(fenceRepair.ToString() == "Types.Invoice");
+        }
+
+        [Test]
+        public void OperatorsTests()
+        {
+
+        }
+    }
+}
---
+++ CSharpSamples/Types/Structs.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Types
+{
+    struct Point
+    {
+        public int x, y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    };
+
+    struct Transaction
+    {
+        public decimal cost;
+        public int id;
+
+        public Transaction(decimal cost, int id)
+        {
+            this.cost = cost;
+            this.id = id;
+        }
+    };
+
+    [TestFixture]
+    class StructsTests
+    {
+        // struct instances are zero initialized
+        [Test]
+        public void InitializationTest()
+        {
+            // Test custom constructor
+            Point coordinates = new Point(5, 10);
+            Assert.That(coordinates.x, Is.EqualTo(5));
+            Assert.That(coordinates.y, Is.EqualTo(10));
+
+            // Test default constructor
+            Point coordinates2 = new Point();
+            Assert.That(coordinates2.x, Is.EqualTo(0));
+            Assert.That(coordinates2.y, Is.EqualTo(0));
+
+            // test assignment
+            coordinates2 = coordinates;
+            Assert.That(coordinates2.x, Is.EqualTo(5));
+            Assert.That(coordinates2.y, Is.EqualTo(10));
+
+
+            // Test custom constructor
+            var transaction = new Transaction(25.75m, 1);
+            Assert.That(transaction.id, Is.EqualTo(1));
+            Assert.That(transaction.cost, Is.EqualTo(25.75m));
+
+            // Test default constructor
+            var transaction2 = new Transaction();
+            Assert.That(transaction2.id, Is.EqualTo(0));
+            Assert.That(transaction2.cost, Is.EqualTo(0m));
+
+            // test assignment
+            transaction2 = transaction;
+            Assert.That(transaction2.id, Is.EqualTo(1));
+            Assert.That(transaction2.cost, Is.EqualTo(25.75m));
+        }
+    }
+}
