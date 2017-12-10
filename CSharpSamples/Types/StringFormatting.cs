a0063cc24af0250fbc7464192a4f06ab401bd231
sfox <kempofighter@gmail.com>
2017-12-10 12:15:07 -0800

    Add a new discovery project for USB devices with logging

---
+++ CSharpSamples/Types/StringFormatting.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Types
+{
+    [TestFixture]
+    class StringFormattingTests
+    {
+        // struct instances are zero initialized
+        [Test]
+        public void StringInterpolationTest()
+        {
+            var p = new Person();
+            p.FirstName = "Shawn";
+            p.LastName = "Fox";
+
+            var fullName = $"{p.FirstName} {p.LastName}";
+            Assert.That(fullName, Is.EqualTo("Shawn Fox"));
+        }
+    }
+}
