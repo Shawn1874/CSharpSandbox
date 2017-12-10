0b08e92462027d6d4565fbc267949b181ba3afc2
Shawn1874 <kempofighter@gmail.com>
2017-12-10 13:29:23 -0800

    Move string testing into the strings project.

---
+++ CSharpSamples/Strings/StringExtMethods.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace Types
+{
+    [TestFixture]
+    class StringExtMethodsTests
+    {
+        /// <summary>
+        /// Assert that the Any extension method also works for strings.
+        /// </summary>
+        [Test]
+        public void StringAnyTest()
+        {
+            var emptyString = "";
+            Assert.That(emptyString.Any() == false);
+        }
+
+        [Test]
+        public void StringReverseTest()
+        {
+            var test = "12345";
+            var reversed = test.Reverse();
+            Assert.That(reversed, Is.EqualTo("54321"));
+        }
+
+    }
+}
