b5c58eb6d8f9af3179c6efc535233a3085b617f1
sfox <kempofighter@gmail.com>
2018-07-04 14:41:47 -0700

    Change project setings to use full debug generation, and add some more tests.

--- CodeSamplesCpp/AlgorithmsTests/TestCustomAlgorithms.cpp
+++ CodeSamplesCpp/AlgorithmsTests/TestCustomAlgorithms.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 #include "TestCustomAlgorithms.h"
 #include "Algorithms.h"
+#include <vector>
+#include <deque>
 
 TestCustomAlgorithms::TestCustomAlgorithms()
 {
@@ -17,9 +19,19 @@ TEST_F(TestCustomAlgorithms, TestReverseString)
   std::string result = "shawn";
 
   Algorithms::Reverse(name.begin(), name.end());
-  //std::reverse(name.begin(), name.end());
 
   EXPECT_EQ(name, result);
+
+}
+
+TEST_F(TestCustomAlgorithms, TestReverseVector)
+{
+  std::vector<int> original = { 9, 7, 5, 3, 1 };
+  std::vector<int> expected = { 1, 3, 5, 7, 9 };
+
+  Algorithms::Reverse(original.begin(), original.end());
+
+  EXPECT_EQ(original, expected);
 
 }
 
@@ -34,4 +46,24 @@ TEST_F(TestCustomAlgorithms, TestMergeSortedRange)
   EXPECT_EQ(a[5], 9);
   EXPECT_EQ(a[7], 12);
   EXPECT_EQ(a.size(), 8);
+
+
+  std::deque<int> c = { 1, 1, 7, 11, 0, 0, 0, 0 };
+  std::deque<int> d = { 2, 3, 9, 12 };
+
+  Algorithms::MergeSortedRanges(d.begin(), d.end(), c.begin(), c.end() + 4);
+  EXPECT_EQ(c[0], 1);
+  EXPECT_EQ(c[1], 1);
+  EXPECT_EQ(c[2], 2);
+  EXPECT_EQ(c[3], 3);
+  EXPECT_EQ(c[4], 7);
+  EXPECT_EQ(c[5], 9);
+  EXPECT_EQ(c[6], 11);
+  EXPECT_EQ(c[7], 12);
+  EXPECT_EQ(c.size(), 8);
+
+  EXPECT_EQ(d[0], 2);
+  EXPECT_EQ(d[1], 3);
+  EXPECT_EQ(d[2], 9);
+  EXPECT_EQ(d[3], 12);
 }
